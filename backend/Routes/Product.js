57d65c9383b138034122f4bba7d93d7068b0fc74
zulfuqarov <zulfuqarov.nebi@gmail.com>
2024-08-16 02:28:48 +0400

    backend adding product in database

--- backend/Routes/Product.js
+++ backend/Routes/Product.js
@@ -33,9 +33,45 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.post("/ProductPush/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { subcategory } = req.body;
+    const product = await Product.findByIdAndUpdate(
+      id,
+      {
+        $addToSet: { subcategory },
+      },
+      { new: true }
+    ).populate("subcategory");
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/ProductPull/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { subcategory } = req.body;
+    const product = await Product.findByIdAndUpdate(
+      id,
+      {
+        $pull: { subcategory: { $in: subcategory } },
+      },
+      { new: true }
+    ).populate("subcategory");
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 router.get("/", async (req, res) => {
   try {
-    const allProduct = await Product.find().populate("subcategory ")
+    const allProduct = await Product.find().populate("subcategory");
     res.status(200).json(allProduct);
   } catch (error) {
     console.log(error);
@@ -43,4 +79,14 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletingProduct = await Product.findByIdAndDelete(id);
+    res.json(deletingProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
 export default router;
