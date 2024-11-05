a826276262d84def8b67e693c0a2bc256a572d1b
zulfuqarov <zulfuqarov.nebi@gmail.com>
2024-11-05 12:34:53 +0400

    deploy:workflows

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import envCompatible from "vite-plugin-env-compatible";
 export default defineConfig({
   envPrefix: "REACT_APP_",
   plugins: [react(), envCompatible()],
+  base: "/Reustorant-Food/"
 });
